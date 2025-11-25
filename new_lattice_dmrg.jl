29aca1b25d694178d63aa22baff1c7ba8c0d62cf
Chenxi_windows <chenxi.liu@princeton.edu>
2025-11-25 15:23:52 -0500

    code

--- new_lattice_dmrg.jl
+++ new_lattice_dmrg.jl
@@ -348,7 +348,7 @@ function run_scan(lattice::Vector{LatticeBond},
 
         # 3) Measurements
         Jc = compute_total_chiral_current(psi_ws, lattice, L, J_parallel, χ)
-        Jr = compute_average_rung_current(psi, lattice, L, J_perp)
+        Jr = compute_average_rung_current(psi_ws, lattice, L, J_perp)
 
         # 4) Optional: rung current correlator at χ ≈ π
         if isapprox(χ, Base.MathConstants.pi; atol=1e-8)
